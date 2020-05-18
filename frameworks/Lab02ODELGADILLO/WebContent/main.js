e9fb1dfaa2bb5713e79fe10bacc6d5338ca7449b
devpianist <oscardelgadillodev@outlook.com>
2020-05-18 13:45:25 -0500

    avance

--- frameworks/Lab02ODELGADILLO/WebContent/main.js
+++ frameworks/Lab02ODELGADILLO/WebContent/main.js
@@ -1,12 +1,7 @@
 function validarTexto() {
-    let text = 'Oscar@';
-    let patronNumeros = /\d/;
-    let patronLetras = /\w/;
-    let error = false;
-    if (patronNumeros.test(text)) {
-        error = true;
-        console.log('HAY NUMEROS!!! O SIGNOS ESPECIALES XD');
-    } else if (patronLetras.test(text)) {
+    let text = 'Osáar';
+    let patronLetras = /^[a-zA-ZñÑáéíóúÁÉÍÓÚ]*$/;
+    if (patronLetras.test(text)) {
         console.log('SOLO HAY LETRAS');
     } else {
         console.log('RARO');
